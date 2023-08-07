624112ec5827e5528c761117bd3bf87117e0872c
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-08-07 20:39:43 -0300

    Form Validated with zod and React Hook Form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import { FormContainer, ContainerLogo } from './styles'
 import formIcon from '../../assets/img/form_icon.png'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+const schema = zod.object({
+  iname: zod.string().min(3, 'Name is required!').nonempty(),
+  iphone: zod
+    .string()
+    .regex(/^(?!0+$)\d{8,}$/, 'Invalid phone number!')
+    .min(11, 'Type at least eleven characters!')
+    .max(11, 'Type no more than 11 characters!'),
+  iemail: zod.string().email('Invalid email!'),
+  icompany: zod.string().min(3, 'Type at least three characters!').nonempty(),
+  iemployees: zod
+    .number()
+    .min(1, 'Minimum value is 1!')
+    .max(1000, 'Maximum value is to big!')
+    .int(),
+  ibusiness: zod.string().min(3, 'Type at least three characters!').nonempty(),
+  iobjectives: zod
+    .string()
+    .min(3, 'Type at least three characters!')
+    .nonempty(),
+})
 
 export function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  })
+
+  console.log(errors)
+
+  const onSubmit = (data: any) => {
+    console.log('Form data: ', data)
+  }
+
   return (
-    <form action="">
+    <form action="" onSubmit={handleSubmit(onSubmit)}>
       <FormContainer>
         <ContainerLogo>
           <h3>Form Company</h3>
@@ -14,56 +52,68 @@ export function Form() {
         <label htmlFor="iname">Name</label>
         <input
           type="text"
-          name="name"
           id="iname"
+          min={1}
+          max={60}
           placeholder="How you prefer to be called?"
+          {...register('iname')}
         />
 
         <label htmlFor="iphone">Phone</label>
         <input
           type="tel"
-          name="phone"
+          min={11}
+          max={11}
           id="iphone"
           placeholder="Enter your WhatsApp number"
+          {...register('iphone')}
         />
 
         <label htmlFor="iemail">E-mail</label>
         <input
           type="email"
-          name="email"
+          min={1}
+          max={60}
           id="iemail"
           placeholder="Enter your email address"
+          {...register('iemail')}
         />
 
         <label htmlFor="icompany">Name of Company</label>
         <input
           type="text"
-          name="company"
+          min={1}
+          max={60}
           id="icompany"
           placeholder="What is the name of the company"
+          {...register('icompany')}
         />
 
         <label htmlFor="iemployees">Number of employees</label>
         <input
           type="number"
-          name="employees"
           id="iemployees"
           placeholder="Enter the number of employees"
+          {...register('iemployees', { valueAsNumber: true })}
         />
 
         <label htmlFor="ibusiness">About your business</label>
         <input
           type="text"
-          name="business"
+          min={1}
+          max={80}
           id="ibusiness"
           placeholder="Tell us a bit about your products or services"
+          {...register('ibusiness')}
         />
 
         <label htmlFor="iobjectives">Objectives of the project</label>
         <textarea
-          name="objectives"
           id="iobjectives"
+          min={1}
+          max={100}
           placeholder="Describe what the objectives of this project are"
+          {...register('iobjectives')}
         ></textarea>
 
         <button type="submit">send proposal</button>
